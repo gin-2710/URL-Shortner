5e12d0d8810c47ab7e10d7081de3037a95b79644
gin-2710 <sreedevishine2002@gmail.com>
2025-02-16 17:33:03 +0530

    Shorten API Handler

--- server/index.js
+++ server/index.js
@@ -2,16 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import apiRoutes from "./api/routes.js";
 import connectDB from "./db/db.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJson from "./swagger.json" with { type: 'json' };
 
 const app = express();
 dotenv.config();
 const PORT = dotenv.PORT || 3000;
 
-await connectDB();
+app.use(express.json());
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`); 
 });
+
+await connectDB();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok", message: "Server is running" });
