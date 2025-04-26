61b733f97be018b98ce83226b7a4be46473d8afc
gin-2710 <sreedevishine2002@gmail.com>
2025-04-26 19:32:50 +0530

    Fix for UUID package not having default export

--- server/api/shorten/alias-handler.js
+++ server/api/shorten/alias-handler.js
@@ -1,30 +1,35 @@
 import Url from "../../models/Url.js";
 import Event from "../../models/Event.js";
-import uuidv4 from "uuid";
+import { v4 as uuid } from "uuid";
 import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 function getDeviceType(userAgent) {
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      userAgent
+    );
   return isMobile ? "mobile" : "desktop";
 }
 
 async function logEvent(userId) {
-  const resIp = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IP_ADDRESS_GEO_LOC_API_KEY}`);
+  const resIp = await axios.get(
+    `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IP_ADDRESS_GEO_LOC_API_KEY}`
+  );
   console.log("Response IP: ", resIp);
 
   const event = new Event({
-    id: uuidv4(),
+    id: uuid(),
     ip: resIp.data.ipAddress,
     deviceType: "mobile", //get device type,
-    
+
     shortUrl: `${process.env.SHORT_URL_BASE}` + req.params.alias,
-//    operatingSystem: get operating system
+    //    operatingSystem: get operating system
     userId: userId,
-    eventTs: new Date().toISOString()
-  })
+    eventTs: new Date().toISOString(),
+  });
 }
 
 const shortenAliasHandler = (req, res) => {
@@ -32,7 +37,6 @@ const shortenAliasHandler = (req, res) => {
     if (err) {
       res.status(404).json({ message: "URL not found" });
     } else {
-
       res.redirect(url.longUrl);
     }
   });
